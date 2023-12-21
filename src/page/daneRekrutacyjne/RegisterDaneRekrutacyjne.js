5d68f765486efafa827caa2626b8efa186508d52
Woojciech <wojtek@suszko.pl>
2023-12-21 12:04:59 +0100

    introduce unified axios client

--- src/page/daneRekrutacyjne/RegisterDaneRekrutacyjne.js
+++ src/page/daneRekrutacyjne/RegisterDaneRekrutacyjne.js
@@ -1,6 +1,18 @@
 import React from 'react'
+import { useEffect } from 'react';
+import axiosPublic from '../../utils/useAxios';
 
 const RegisterDaneRekrutacyjne = () => {
+  const client = axiosPublic;
+
+  useEffect(() => {
+        const fetchFormData = async () => {
+            const formData = await client.get("/api/v1/dane-rekrutacyjne/form-data");
+            console.log(formData.data)
+        }
+        fetchFormData();
+  })
+
   return (
     <div>
         <h1>Register Dane Rekrutacyjne</h1>
--- src/page/kierunek/RegisterKierunek.js
+++ src/page/kierunek/RegisterKierunek.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { useEffect } from 'react'
+import axiosPublic from '../../utils/useAxios';
 
 const RegisterKierunek = () => {
-  return (
+    const client = axiosPublic;
+
+    useEffect(() => {
+        
+    })
+
+    return (
     <div>
         <h1>Register Kierunek</h1>
     </div>
