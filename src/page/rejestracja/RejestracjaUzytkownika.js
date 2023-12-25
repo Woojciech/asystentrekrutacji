b63a39c2e9c01344a265d3690744fd8388aa75b5
MDybek <dybek8780@gmail.com>
2023-12-25 20:12:37 +0100

    some styles applied on create new account page

--- src/page/rejestracja/RejestracjaUzytkownika.js
+++ src/page/rejestracja/RejestracjaUzytkownika.js
@@ -37,12 +37,13 @@ const RegisterUser = () => {
     return (
         <div className="split-container">
             <div className="logo-container">
-                <img src={require('../../photos/logo-PWr-pion-bez-tla-207x300.png')} alt="Logo"/>
+                <img src={require('../../photos/logo-PWr-pion-bez-tla-207x300.png')} alt="Logo"
+                     style={{width: '50%', height: 'auto'}}/>
             </div>
-            <div>
+            <div className={"form-container"}>
                 <h1 className="page-info">Utwórz nowe konto</h1>
                 <form>
-                    <div style={{display: 'flex', flexDirection: 'column', gap: '10px'}}>
+                    <div style={{display: 'flex', flexDirection: 'column'}}>
                         <input type="text" name="imie" value={userData.imie} onChange={handleInputChange}
                                placeholder="Imię"/>
                         <input type="text" name="nazwisko" value={userData.nazwisko} onChange={handleInputChange}
@@ -57,15 +58,17 @@ const RegisterUser = () => {
                                placeholder="PESEL"/>
                         <input type="text" name="numerTelefonu" value={userData.numerTelefonu}
                                onChange={handleInputChange} placeholder="Numer Telefonu"/>
-                        <div className="form-double-column" style={{display: 'flex', gap: '10px'}}>
-                            <div style={{display: 'flex', flexDirection: 'column', gap: '2px'}}>
+                        <div className="form-double-column" style={{display: 'flex', gap: '20px'}}>
+                            <div style={{display: 'flex', flexDirection: 'column'}}>
                                 <label className="form-info">Data Urodzenia:</label>
-                                <input className={"input-birth-date"} type="date" name="dataUrodzenia" value={userData.dataUrodzenia}
+                                <input className={"input-birth-date"} type="date" name="dataUrodzenia"
+                                       value={userData.dataUrodzenia}
                                        onChange={handleInputChange} placeholder="Data Urodzenia"/>
                             </div>
-                            <div style={{display: 'flex', flexDirection: 'column', gap: '2px'}}>
+                            <div style={{display: 'flex', flexDirection: 'column'}}>
                                 <label className="form-info">Płeć:</label>
-                                <select  className={"input-plec"} name="plec" value={userData.plec} onChange={handleInputChange}>
+                                <select className={"input-plec"} name="plec" value={userData.plec}
+                                        onChange={handleInputChange}>
                                     <option value="MEZCZYZNA">Mężczyzna</option>
                                     <option value="KOBIETA">Kobieta</option>
                                 </select>
@@ -73,6 +76,10 @@ const RegisterUser = () => {
                         </div>
                     </div>
                     <button type="button" onClick={handleSubmit}>Zarejestruj</button>
+                    <div className="line-below-button"></div>
+                    <p className="login-text">
+                        Masz już konto? <a href="/" className="login-link">Zaloguj się</a>
+                    </p>
                 </form>
             </div>
         </div>
